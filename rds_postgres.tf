e8eb910df0dbef80c93e6e8a49ef3999cf3c67da
Caio <caio.cb.sp@gmail.com>
2024-05-05 15:19:34 -0300

    Provising DB in AWS as code using Terraform

---
+++ rds_postgres.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "tech_challenge_postgres_instance" {
+  identifier           = "tech-challenge-postgres-db"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "16.1"
+  instance_class       = "db.t3.micro"
+  db_name              = var.DB_NAME
+  username             = var.DB_USER
+  password             = var.DB_PASSWORD
+  parameter_group_name = var.PARAM_GROUP_NAME
+}
